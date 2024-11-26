32a7d568a2b20f5066b67863fc66f3beb5e23b68
rajpootGovind <govindrajpoot12000@gmail.com>
2024-11-26 12:42:41 +0530

    after installing express, dotenv , start the server successfully

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+
+const express = require("express");
+
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+
+// Start the server
+app.listen(PORT, (err) => {
+    console.log(`get error ${err}`);
+    
+  console.log(`Server running successfully on port ${PORT}`);
+  
+});

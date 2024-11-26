b752404f76f4687bac2ef4d35e9a8278c4bd6111
rajpootGovind <govindrajpoot12000@gmail.com>
2024-11-26 13:49:42 +0530

    API issue getting error during generating keywords

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,22 @@ const express = require("express");
 
 const dotenv = require("dotenv");
 
+const generateRoutes = require("./Routes/route.generate")
+
 
 dotenv.config();
 const app = express();
 
+app.use(express.json())
+
 const PORT = process.env.PORT;
+
+app.use("/api/generate", generateRoutes);
 
 
 // Start the server
-app.listen(PORT, (err) => {
-    console.log(`get error ${err}`);
+app.listen(PORT, () => {
+    // console.log(`get error ${err}`);
     
   console.log(`Server running successfully on port ${PORT}`);
   
